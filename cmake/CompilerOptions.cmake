768d28b65f9e1fb54a21c58f16e74f3bad74724b
Emre Ozbay <emre.ozbay@piworks.net>
2022-10-03 14:41:30 +0300

    build: add cmake files

---
+++ cmake/CompilerOptions.cmake
@@ -0,0 +1,17 @@
+set(COMPILE_OPTIONS
+    -Wall
+    -Wextra
+    -Wshadow
+    -Wnon-virtual-dtor
+    -Wold-style-cast
+    -Wcast-align
+    -Wunused
+    -Woverloaded-virtual
+    -Wpedantic
+    -Wconversion
+    -Wsign-conversion
+    -Wnull-dereference
+    -Wdouble-promotion
+    -Wformat=2
+    # -Werror
+)
