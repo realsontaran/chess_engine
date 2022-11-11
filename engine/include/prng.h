81997ef13c627ec2a2749d525f5569db4e157807
Emre Ozbay <emre.ozbay@piworks.net>
2022-11-11 20:37:20 +0300

    feat: magic number generator

--- engine/include/prng.h
+++ engine/include/prng.h
@@ -1,6 +1,9 @@
 #ifndef PRNG_H_
 #define PRNG_H_
+#include <bitboard.h>
 
 // https://en.wikipedia.org/wiki/Xorshift#Example_implementation
-unsigned int get_random();
+unsigned int get_random_U32();
+U64 get_random_U64();
+U64 generate_magic_number();
 #endif // PRNG_H_
--- engine/src/prng.cpp
+++ engine/src/prng.cpp
@@ -1,8 +1,10 @@
-#include "prng.h"
+#include <prng.h>
+
+// https://www.chessprogramming.org/Looking_for_Magics#Feeding_in_Randoms
 
 static unsigned int state = 1804289383; // Random start state
 
-unsigned int get_random() {
+unsigned int get_random_U32() {
     // get current state
     unsigned int number = state;
 
@@ -16,4 +18,25 @@ unsigned int get_random() {
 
     // return random number
     return number;
+}
+
+U64 get_random_U64() {
+    U64 n1;
+    U64 n2;
+    U64 n3;
+    U64 n4;
+
+    // init random numbers slicing 16 bits from MS1B side
+    n1 = static_cast<U64>((get_random_U32() & 0xFFFF));
+    n2 = static_cast<U64>((get_random_U32() & 0xFFFF));
+    n3 = static_cast<U64>((get_random_U32() & 0xFFFF));
+    n4 = static_cast<U64>((get_random_U32() & 0xFFFF));
+
+    // return random number
+    return n1 | (n2 << 16) | (n3 << 32) | (n4 << 48);
+}
+
+// generate magic number candidate
+U64 generate_magic_number() {
+    return get_random_U64() & get_random_U64() & get_random_U64(); // NOLINT
 }
