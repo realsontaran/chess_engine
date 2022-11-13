d3ba513b8a17754533d35841c1f15dc1a5818c15
Emre Ozbay <emre.ozbay@piworks.net>
2022-11-13 23:34:14 +0300

    refactor: formatting

--- engine/include/bitboard.h
+++ engine/include/bitboard.h
@@ -4,64 +4,62 @@
 typedef unsigned long long U64;
 
 namespace Bitboard {
-constexpr void set_bit(U64 &board, int sq) {
-    (board) |= (1ULL << (sq));
-}
+    constexpr void set_bit(U64 &board, int sq) {
+        (board) |= (1ULL << (sq));
+    }
 
-constexpr U64 get_bit(U64 board, int sq) {
-    return (board) & (1ULL << (sq));
-}
+    constexpr U64 get_bit(U64 board, int sq) {
+        return (board) & (1ULL << (sq));
+    }
 
-constexpr void pop_bit(U64 &board, int sq) {
-    (board) &= ~(1ULL << (sq));
-}
+    constexpr void pop_bit(U64 &board, int sq) {
+        (board) &= ~(1ULL << (sq));
+    }
 
-constexpr unsigned int bit_count(U64 board) {
-    unsigned int count;
-    for (count = 0; board != 0ULL; ++count, board &= board - 1)
-        ;
-    return count;
-}
+    constexpr unsigned int bit_count(U64 board) {
+        unsigned int count;
+        for (count = 0; board != 0ULL; ++count, board &= board - 1)
+            ;
+        return count;
+    }
 
-constexpr unsigned int get_lsb(U64 board) {
-    return (board != 0ULL) ? bit_count((board & -board) - 1) : 0;
-}
+    constexpr unsigned int get_lsb(U64 board) {
+        return (board != 0ULL) ? bit_count((board & -board) - 1) : 0;
+    }
 
-// clang-format off
-enum Square {
-    a8, b8, c8, d8, e8, f8, g8, h8,
-    a7, b7, c7, d7, e7, f7, g7, h7,
-    a6, b6, c6, d6, e6, f6, g6, h6,
-    a5, b5, c5, d5, e5, f5, g5, h5,
-    a4, b4, c4, d4, e4, f4, g4, h4,
-    a3, b3, c3, d3, e3, f3, g3, h3,
-    a2, b2, c2, d2, e2, f2, g2, h2,
-    a1, b1, c1, d1, e1, f1, g1, h1, no_sq
-};
-// static const char *square_strings[] = {
-//     "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
-//     "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
-//     "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
-//     "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
-//     "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
-//     "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
-//     "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
-//     "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"
-// };
+    // clang-format off
+    enum Square {
+        a8, b8, c8, d8, e8, f8, g8, h8,
+        a7, b7, c7, d7, e7, f7, g7, h7,
+        a6, b6, c6, d6, e6, f6, g6, h6,
+        a5, b5, c5, d5, e5, f5, g5, h5,
+        a4, b4, c4, d4, e4, f4, g4, h4,
+        a3, b3, c3, d3, e3, f3, g3, h3,
+        a2, b2, c2, d2, e2, f2, g2, h2,
+        a1, b1, c1, d1, e1, f1, g1, h1, no_sq
+    };
+    static const char *square_strings[] = {
+        "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+        "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+        "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+        "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+        "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+        "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+        "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+        "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"
+    };
 
-// clang-format on
+    // clang-format on
 
-enum Side { white, black, both };
+    enum Side { white, black, both };
 
-enum Ascii { P, N, B, R, Q, K, p, n, b, r, q, k };
+    /* Magic numbers for excluding files for attack mask generation */
+    constexpr U64 notAFile = 0xfefefefefefefefe;
+    constexpr U64 notAorBFile = 0xfcfcfcfcfcfcfcfc;
+    constexpr U64 notHFile = 0x7f7f7f7f7f7f7f7f;
+    constexpr U64 notHorGFile = 0x3f3f3f3f3f3f3f3f;
 
-/* Magic numbers for excluding files for attack mask generation */
-constexpr U64 notAFile = 0xfefefefefefefefe;
-constexpr U64 notAorBFile = 0xfcfcfcfcfcfcfcfc;
-constexpr U64 notHFile = 0x7f7f7f7f7f7f7f7f;
-constexpr U64 notHorGFile = 0x3f3f3f3f3f3f3f3f;
-
-void printBoard(U64 board);
+    void printBoard(U64 board);
 
 } // namespace Bitboard
 
