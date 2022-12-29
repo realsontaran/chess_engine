976c676f861026bcc78fb011a268cfadec87a481
Emre Ozbay <emre.ozbay@piworks.net>
2022-12-29 19:05:28 +0300

    feat:: add MoveList class for storing moves

---
+++ engine/include/move_list.hpp
@@ -0,0 +1,35 @@
+#ifndef MOVE_LIST_H_
+#define MOVE_LIST_H_
+#include <vector>
+#include <iostream>
+#include <encoding.hpp>
+
+class MoveList {
+    // TODO Maybe add UCI
+    // https://www.youtube.com/watch?v=AINYYiV-83I&list=PLmN0neTso3Jxh8ZIylk74JpwfiWNI76Cs&index=29
+  public:
+    MoveList() {
+    }
+
+    ~MoveList() {
+    }
+
+    void print() {
+        for (auto const &move : moves) {
+            std::cout << move.toString() << std::endl;
+        }
+    }
+
+    void add(Move const &move) {
+        moves.push_back(move);
+    }
+
+    size_t getSize() {
+        return moves.size();
+    }
+
+  private:
+    std::vector<Move> moves;
+};
+
+#endif // MOVE_LIST_H_
